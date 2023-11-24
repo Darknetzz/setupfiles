68a6835067cbfe8f77355a54acfa39eeb0f3f265
Kristian Røste <github@roste.org>
2023-11-24 12:51:23 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# This file is supposed to setup a fresh computer with some presets like a template
+
+#   - Determine OS
+#   - Windows activator
+#   - Debloater
+#   - Aliases
+#   - Automatically fetch ninite/application package(s) - github?
+# 	  - PHP
+# 	  - Powershell
+# 	  - Python
+# 	  - Brave
+# 	  - FileZilla
+# 	  - RDP
+#     - Windows Terminal
